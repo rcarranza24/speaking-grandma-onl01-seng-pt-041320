1fa0c08b0881eb093fd402cd6a7c9ddbc94929e9
Rosa Carranza <rcarranza1024@yahoo.com>
2020-04-14 23:05:32 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,3 +1,7 @@
 def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
-    puts "I LOVE YOU TOO PUMPKIN!"
+    return "I LOVE YOU TOO PUMPKIN!"
+  elsif phrase.downcase
+    return "HUH?! SPEAK UP, SONNY!"
+  elsif phrase.upcase
+    return "NO, NOT SINCE 1938!"
